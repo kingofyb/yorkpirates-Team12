221da3a729d0c79ce05c9d677d9f8861a45a2fa8
Jacob Poulton <jp1808@york.ac.uk>
2022-01-31 15:56:22 +0000

    Changed fullscreen to forced maximised.

--- lwjgl3/src/main/java/com/engteam14/yorkpirates/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/engteam14/yorkpirates/lwjgl3/Lwjgl3Launcher.java
@@ -25,7 +25,9 @@ public class Lwjgl3Launcher {
 		//// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
 		//// useful for testing performance, but can also be very stressful to some hardware.
 		//// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-		configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+
+		configuration.setMaximized(true);
+		configuration.setResizable(false);
 
 		configuration.setWindowIcon("icon_128.png", "icon_64.png", "icon_32.png", "icon_16.png");
 
