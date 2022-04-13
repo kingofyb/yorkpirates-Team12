68fe85cc8f5d880155e1eb2420713b19e886ab1e
yellow-flamingo <amymason1211@gmail.com>
2022-04-13 21:40:25 +0100

    can now run tests

---
+++ tests/src/main/java/com/engteam14/yorkpirates/tests/AssetTests.java
@@ -0,0 +1,15 @@
+package com.engteam14.yorkpirates.tests;
+
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+
+    @Test
+    public void testShipAssetExists() {
+        assertTrue(true);
+    }
+}
